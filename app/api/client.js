79dba6f12bd0e3dc7dc43130be20aed28f3f549f
Biswanath Tewari <biswanath@geekyants.com>
2021-09-14 20:01:56 +0530

    caching without redux

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,24 @@
 import {create} from 'apisauce';
 
+import cache from '../utility/cache';
+
 const apiClient = create({
   baseURL: 'https://randomuser.me',
 });
+
+const get = apiClient.get;
+
+//^ altering the get()
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+
+  if (response.ok) {
+    await cache.store(url, response.data); //* caching the response
+    return response;
+  }
+
+  const data = await cache.get(url); //* retrieving the data from the cache
+  return data ? {ok: true, data} : response;
+};
 
 export default apiClient;
